58c7a585b9fd15640c03ae26047ac7d4ab2c7680
Aulbn <38533964+Aulbn@users.noreply.github.com>
2021-05-03 00:35:53 +0200

    First Commit

---
+++ Assets/Scripts/QuickSceneLoader.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class QuickSceneLoader : MonoBehaviour
+{
+    public GameObject loadScreen;
+    public Image loadingBar;
+    public bool isLoading;
+
+    [Header("Scenes")]
+    public SceneLoadObject[] scenes;
+
+    [Serializable]
+    public struct SceneLoadObject
+    {
+        public string[] sceneName;
+    }
+
+    private Keyboard kb;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        kb = InputSystem.GetDevice<Keyboard>();
+        loadScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isLoading)
+            return;
+
+        if (kb.numpad1Key.wasPressedThisFrame)
+            LoadLevel(0);
+        if (kb.numpad2Key.wasPressedThisFrame)
+            LoadLevel(1);
+        if (kb.numpad3Key.wasPressedThisFrame)
+            LoadLevel(2);
+        if (kb.numpad4Key.wasPressedThisFrame)
+            LoadLevel(3);
+        if (kb.numpad5Key.wasPressedThisFrame)
+            LoadLevel(4);
+    }
+
+    private void LoadLevel(int index)
+    {
+        Debug.Log("Load level: " + (index + 1));
+        loadScreen.SetActive(true);
+        isLoading = true;
+        StartCoroutine(LoadScenesAsync(scenes[index].sceneName));
+    }
+
+    private IEnumerator LoadScenesAsync(string[] scenes)
+    {
+        bool isDone;
+        float progress;
+        AsyncOperation[] operations = new AsyncOperation[scenes.Length];
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            operations[i] = SceneManager.LoadSceneAsync(scenes[i], i == 0 ? LoadSceneMode.Single : LoadSceneMode.Additive);
+        }
+
+        do {
+            progress = 0;
+            isDone = true;
+
+            foreach (AsyncOperation operation in operations)
+            {
+                if (!operation.isDone)
+                {
+                    isDone = false;
+                }
+                progress += Mathf.Clamp01(operation.progress / .9f / scenes.Length);
+            }
+
+            loadingBar.fillAmount = progress;
+            yield return null;
+
+        } while (!isDone);
+
+        loadScreen.SetActive(false);
+        isLoading = false;
+        Debug.Log("Done Loading");
+    }
+
+}
---
+++ Assets/Scripts/SceneLoad/SceneLoadManager.cs
@@ -0,0 +1,258 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+enum sceneAmmount {singleScene, multipleScenes}
+
+public class SceneLoadManager : MonoBehaviour
+{
+    private static SceneLoadManager instance = null;
+
+    //scene fade out
+    public delegate void OnSceneChangeStart();
+    public static OnSceneChangeStart OnChangeScene;
+
+    //scene fade in
+    public delegate void OnReturnToSceneStart();
+    public static OnReturnToSceneStart OnReturnToScene;
+
+    //scene instant fade out
+    public delegate void OnReloadScene();
+    public static OnReloadScene OnRestartScene;
+
+    //temporary scene variables
+    string tempFirstSceneLoaded;
+    string[] tempSceneNameList;
+    private bool isLoadingMultiScene = false;
+
+    private List<string> tempCurrentScenes = new List<string>();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+            Destroy(gameObject);
+
+    }
+
+    void OnEnable()
+    {
+        SceneLoadTrigger.onEnterSceneBound += OnLoadSubScenes;
+        SceneLoadTrigger.onExitSceneBound += OnUnloadSubScenes;
+        SceneLoadTrigger.onEnterNewSceneBound += OnSceneTransition;
+        SceneLoadTrigger.onMergeAwakeScene += OnMergeStartScene;
+        PauseMenu.OnRestartScene += OnResetScene;
+    }
+
+    void OnDisable()
+    {
+        SceneLoadTrigger.onEnterSceneBound -= OnLoadSubScenes;
+        SceneLoadTrigger.onExitSceneBound -= OnUnloadSubScenes;
+        SceneLoadTrigger.onEnterNewSceneBound -= OnSceneTransition;
+        SceneLoadTrigger.onMergeAwakeScene -= OnMergeStartScene;
+        PauseMenu.OnRestartScene -= OnResetScene;
+    }
+
+    void OnMergeStartScene(string sceneString)
+    {
+        StartCoroutine(MergeTheAwakeScene(sceneString));
+    }
+
+    public void OnResetScene()
+    {
+
+        OnRestartScene?.Invoke(); //Tell scene fade to set the alpha to 1
+        List<string> tempCurrentScenesName = new List<string>();
+        //GET CURRENT SCENES
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            tempCurrentScenesName.Add(SceneManager.GetSceneAt(i).name.ToString());
+        }
+
+        //Create temporary scene(because you must have atleast 1 scene active)
+        SceneManager.CreateScene("temp");
+
+        //unload all scenes except the newly created one
+        for (int i = 0; i < SceneManager.sceneCount - 1; i++)
+        {
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i));
+        }
+
+        //start loading the first scene
+        LoadSceneSingle(tempCurrentScenesName[0]);
+
+        //load rest
+        if (tempCurrentScenesName.Count > 1)
+        {
+            for (int i = 1; i < tempCurrentScenesName.Count; i++)
+            {
+                LoadSceneAdditive(tempCurrentScenesName[i]);
+            }
+        }
+ 
+        //very crude fix for a bug(curos visible after restart)
+        if (tempCurrentScenesName[0].ToString() != SceneManager.GetSceneByBuildIndex(0).name)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+       }
+
+        tempCurrentScenesName.Clear();
+        OnChangeScene?.Invoke();
+
+        Time.timeScale = 1.0f; //very crude fix for a bug(time scale not reset after restart)
+    }
+
+
+    public void LoadSceneSingle(string SceneName)
+    {
+        if (SceneName != null)
+        {
+            if (!SceneManager.GetSceneByName(SceneName).isLoaded)
+            {
+                SceneManager.LoadSceneAsync(SceneName, LoadSceneMode.Single);
+                //Debug.Log(SceneName + " Loaded");
+            }
+        }
+    }
+
+    public void LoadSceneAdditive(string SceneName)
+    {
+        if (!SceneManager.GetSceneByName(SceneName).isLoaded)
+            {
+                SceneManager.LoadSceneAsync(SceneName, LoadSceneMode.Additive);
+                //Debug.Log(SceneName + " Loaded");
+            }
+    }
+    public void UnloadScene(string SceneName)
+    {
+        if (SceneManager.GetSceneByName(SceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(SceneName);
+            //Debug.Log(SceneName + " Unloaded");
+        }
+    }
+
+
+    public void OnLoadSubScenes(string theScene, string[] multiSceneLoadList, bool isMultiModeLoad)
+    {
+        if (!isMultiModeLoad)
+        {
+            LoadSceneAdditive(theScene);
+        }
+        else if (isMultiModeLoad)
+        {
+            for (int i = 0; i < multiSceneLoadList.Length; i++)
+            {
+                LoadSceneAdditive(multiSceneLoadList[i].ToString());
+            }
+        }
+
+    }
+
+    public void OnUnloadSubScenes(string theScene, string[] multiSceneLoadList, bool isMultiModeLoad)
+    {
+        if (!isMultiModeLoad)
+        {
+            UnloadScene(theScene);
+        }
+        else if (isMultiModeLoad)
+        {
+            for (int p = 0; p < multiSceneLoadList.Length; p++)
+            {
+                UnloadScene(multiSceneLoadList[p].ToString());
+            }
+        }
+    }
+
+    public void OnSceneTransition(string theScene, string[] multiSceneLoadList, bool isMultiModeLoad)
+    {
+        OnReturnToScene?.Invoke();
+        StartCoroutine(WhileSceneLoad(theScene, multiSceneLoadList, isMultiModeLoad));
+    }
+
+    IEnumerator WhileSceneLoad(string theSceneName, string[] multiSceneList, bool isMultiModeLoad)
+    {
+        yield return null;
+        if (!isMultiModeLoad)
+        {
+            if (theSceneName != null)
+            {
+                tempFirstSceneLoaded = theSceneName;
+            }
+        }
+        else if (isMultiModeLoad)
+        {
+            if (multiSceneList != null)
+            {
+                tempFirstSceneLoaded = multiSceneList[0];
+                tempSceneNameList = multiSceneList;
+            }
+        }
+        AsyncOperation asyncOperator;
+        if (!SceneManager.GetSceneByName(tempFirstSceneLoaded).isLoaded)
+        {
+            asyncOperator = SceneManager.LoadSceneAsync(tempFirstSceneLoaded);
+            asyncOperator.allowSceneActivation = false;
+           // Debug.Log(asyncOperator.progress);
+            yield return new WaitForSecondsRealtime(2);
+           // Debug.Log(asyncOperator.progress);
+
+            if (asyncOperator.progress >= 0.9f)
+            {
+                asyncOperator.allowSceneActivation = true;
+                if (isMultiModeLoad)
+                {
+                    string tempSceneString;
+                    for (int i = 1; i < multiSceneList.Length; i++)
+                    {
+                        tempSceneString = tempSceneNameList[i].ToString();
+                        LoadSceneAdditive(tempSceneString);
+                        yield return new WaitForSecondsRealtime(1);
+                      // SceneManager.MergeScenes(SceneManager.GetSceneByName(tempSceneNameList[i].ToString()), SceneManager.GetSceneByName(tempFirstSceneLoaded));
+                    }
+                    OnChangeScene?.Invoke();    //tells scene fade scene changed
+                }
+                else
+                {
+                    OnChangeScene?.Invoke();
+                }
+            }
+        }
+        //else
+            //Debug.LogWarning(theSceneName + " is already loaded");
+    }
+
+  /* IEnumerator CheckIfLastSceneLoaded()
+    {
+        yield return new WaitForEndOfFrame();
+        while(!SceneManager.GetSceneAt(SceneManager.sceneCount).isLoaded)
+        {
+           // Debug.Log("loading...");
+        }
+        OnChangeScene?.Invoke();
+    }*/
+
+
+    //fix for restart bug(load on awake breaks restart)
+    IEnumerator MergeTheAwakeScene(string sceneString)
+    {
+        AsyncOperation asyncOperationAwake;
+        asyncOperationAwake = SceneManager.LoadSceneAsync(sceneString, LoadSceneMode.Additive);
+        while (!asyncOperationAwake.isDone)
+        {
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+        asyncOperationAwake.allowSceneActivation = true;
+        yield return new WaitForEndOfFrame();
+        SceneManager.MergeScenes(SceneManager.GetSceneByName(sceneString), SceneManager.GetActiveScene());
+        StopAllCoroutines();
+    }
+
+}
